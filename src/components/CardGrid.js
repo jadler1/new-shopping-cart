51562c50c3bd7020ee3c25077ee022c88ad25076
jadler1 <jacksonadler2020@u.northwestern.edu>
2020-01-20 20:52:18 -0600

    added prices

--- src/components/CardGrid.js
+++ src/components/CardGrid.js
@@ -1,8 +1,8 @@
 import 'rbx/index.css';
-import {Card, Content, Column} from 'rbx';
+import {Card, Content, Column, Image} from 'rbx';
 import React from 'react';
 
-const ShirtCard = ({title}) => (
+const ShirtCard = ({title, image, price, priceFormat}) => (
     // <Card>
     //     <Card.Content>
     //         <Content>
@@ -12,9 +12,17 @@ const ShirtCard = ({title}) => (
     // </Card>
     <Column>
         <Card>
-            <Card.Content>
-                <Content>
+            <Card.Header>
+                <Card.Header.Title>
                     {title}
+                </Card.Header.Title>
+            </Card.Header>
+            <Card.Content>
+                <Image.Container size='4by10'>
+                    <Image src={`data/products/${image}_1.jpg`}/>
+                </Image.Container>
+                <Content>
+                    {priceFormat}{price}
                 </Content>
             </Card.Content>
         </Card>
@@ -24,7 +32,10 @@ const ShirtCard = ({title}) => (
 const CardGrid = ({products}) => {
     return (
         <Column.Group vcentered multiline desktop mobile>
-            {products.map(product => <ShirtCard title={product.title}/>)}
+            {products.map(product => <ShirtCard price={product.price} 
+                                                priceFormat={product.currencyFormat} 
+                                                title={product.title} 
+                                                image={product.sku}/>)}
          </Column.Group>
     );
 };
